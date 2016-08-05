f0a2d4288f70e3fda930da51c8a1d168d89d2288
Elec332 <arnout1998@gmail.com>
2016-08-05 15:55:45 +0200

    Added method to fetch the teleporters

--- src/main/java/bitmovers/elementaldimensions/dimensions/PortalDungeonLocator.java
+++ src/main/java/bitmovers/elementaldimensions/dimensions/PortalDungeonLocator.java
@@ -1,10 +1,17 @@
 package bitmovers.elementaldimensions.dimensions;
 
+import bitmovers.elementaldimensions.blocks.portal.PortalDialerTileEntity;
 import bitmovers.elementaldimensions.util.Config;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.common.DimensionManager;
 
+import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.Random;
 
 public class PortalDungeonLocator {
@@ -17,6 +24,27 @@ public class PortalDungeonLocator {
         Random random = new Random((world.getSeed() + chunkX) * 37 + chunkZ * 5 + 113);
         random.nextFloat();
         return random.nextFloat() < Config.Dimensions.portalDungeonChance;
+    }
+
+    @Nullable
+    public static PortalDialerTileEntity getTeleporter(WorldServer currentWorld, BlockPos currentPos, Dimensions dimension){
+        int dim = dimension.getDimensionID();
+        MinecraftServer server = currentWorld.getMinecraftServer();
+        if (server == null){
+            return null;
+        }
+        WorldServer world = server.worldServerForDimension(dim);
+        Chunk chunk = world.getChunkFromBlockCoords(currentPos);
+        for (Map.Entry<BlockPos, TileEntity> entry : chunk.getTileEntityMap().entrySet()){
+            BlockPos tilePos = entry.getKey();
+            if (currentPos.getX() == tilePos.getX() && currentPos.getZ() == tilePos.getZ()){
+                TileEntity tile = entry.getValue();
+                if (tile instanceof PortalDialerTileEntity){
+                    return (PortalDialerTileEntity) tile;
+                }
+            }
+        }
+        return null;
     }
 
 }
