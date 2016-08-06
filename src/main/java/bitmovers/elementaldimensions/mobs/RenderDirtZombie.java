8ee9213b2c47525e34993485702feb42efea19cd
McJty <mcjty1@gmail.com>
2016-08-06 07:06:06 +0200

    Wrong texture

--- src/main/java/bitmovers/elementaldimensions/mobs/RenderDirtZombie.java
+++ src/main/java/bitmovers/elementaldimensions/mobs/RenderDirtZombie.java
@@ -11,7 +11,7 @@ import javax.annotation.Nonnull;
 
 public class RenderDirtZombie extends RenderLiving<EntityDirtZombie> {
 
-    private ResourceLocation mobTexture = new ResourceLocation("elementaldimensions:textures/entity/guard.png");
+    private ResourceLocation mobTexture = new ResourceLocation("elementaldimensions:textures/entity/dirtzombie.png");
 
     public static final Factory FACTORY = new Factory();
 
