c8dd49bd8a7634a7066d4e9ce348ae2620f43a4e
McJty <mcjty1@gmail.com>
2016-08-08 06:25:27 +0200

    Added localized names for the mobs

--- src/main/java/bitmovers/elementaldimensions/init/EntityRegister.java
+++ src/main/java/bitmovers/elementaldimensions/init/EntityRegister.java
@@ -12,12 +12,12 @@ public class EntityRegister {
 
     public static void init() {
         int id = 1;
-        EntityRegistry.registerModEntity(EntityDirtZombie.class, "Dirt Zombie", id++, ElementalDimensions.instance, 64, 3, true, 0x996600, 0x00ff00);
+        EntityRegistry.registerModEntity(EntityDirtZombie.class, "DirtZombie", id++, ElementalDimensions.instance, 64, 3, true, 0x996600, 0x00ff00);
         EntityRegistry.registerModEntity(EntityGuard.class, "Guard", id++, ElementalDimensions.instance, 32, 3, true, 0x880088, 0x00ff00);
-        EntityRegistry.registerModEntity(EntityWaterCreep.class, "Water Creep", id++, ElementalDimensions.instance, 64, 3, true, 0x002288, 0x005533);
-        EntityRegistry.registerModEntity(EntityDirtZombieBoss.class, "Dirt Zombie Boss", id++, ElementalDimensions.instance, 64, 3, true, 0x996600, 0x00ff00);
-        EntityRegistry.registerModEntity(EntityWaterCreepBoss.class, "Water Creep Boss", id++, ElementalDimensions.instance, 64, 3, true, 0x002288, 0x005533);
-        EntityRegistry.registerModEntity(EntityWaterBossSeed.class, "Water Boss Seed", id++, ElementalDimensions.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(EntityWaterCreep.class, "WaterCreep", id++, ElementalDimensions.instance, 64, 3, true, 0x002288, 0x005533);
+        EntityRegistry.registerModEntity(EntityDirtZombieBoss.class, "DirtZombieBoss", id++, ElementalDimensions.instance, 64, 3, true, 0x996600, 0x00ff00);
+        EntityRegistry.registerModEntity(EntityWaterCreepBoss.class, "WaterCreepBoss", id++, ElementalDimensions.instance, 64, 3, true, 0x002288, 0x005533);
+        EntityRegistry.registerModEntity(EntityWaterBossSeed.class, "WaterBossSeed", id++, ElementalDimensions.instance, 64, 3, true);
         EntitySpawnPlacementRegistry.setPlacementType(EntityWaterCreep.class, EntityLiving.SpawnPlacementType.IN_WATER);
         EntitySpawnPlacementRegistry.setPlacementType(EntityWaterCreepBoss.class, EntityLiving.SpawnPlacementType.IN_WATER);
 
