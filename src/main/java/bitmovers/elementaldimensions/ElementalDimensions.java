06f12765b50bf1814a8eaa902574f923dbb8a6cb
McJty <mcjty1@gmail.com>
2016-08-25 12:38:20 +0200

    0.0.3

--- src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
+++ src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
@@ -37,7 +37,7 @@ public class ElementalDimensions {
 
     public static final String MODID = "elementaldimensions";
     public static final String MODNAME = "Elemental Dimensions";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
 
     @SidedProxy(clientSide="bitmovers.elementaldimensions.proxy.ClientProxy", serverSide="bitmovers.elementaldimensions.proxy.ServerProxy")
     public static CommonProxy proxy;
