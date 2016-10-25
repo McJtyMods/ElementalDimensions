ff160e66c2247df81b8ea9e8723533710715d225
McJty <mcjty1@gmail.com>
2016-10-25 08:58:06 +0200

    Comment out the silverfish blocks for now

--- src/main/java/bitmovers/elementaldimensions/ncLayer/overworldTweaks/SilverfishTweak.java
+++ src/main/java/bitmovers/elementaldimensions/ncLayer/overworldTweaks/SilverfishTweak.java
@@ -1,13 +1,6 @@
 package bitmovers.elementaldimensions.ncLayer.overworldTweaks;
 
-import bitmovers.elementaldimensions.init.BlockRegister;
 import bitmovers.elementaldimensions.util.worldgen.RegisteredWorldGenerator;
-import bitmovers.elementaldimensions.util.worldgen.WorldGenHelper;
-import elec332.core.world.WorldHelper;
-import net.minecraft.block.BlockStone;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -23,13 +16,13 @@ public class SilverfishTweak implements IWorldGenerator {
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-        for (int i = 0; i < random.nextInt(40); i++) {
-            BlockPos pos = WorldGenHelper.randomXZPos(chunkX, chunkZ, random.nextInt(64) + 1, random);
-            IBlockState blockState = WorldHelper.getBlockState(world, pos);
-            if (blockState.getBlock() == Blocks.STONE && blockState.getValue(BlockStone.VARIANT) == BlockStone.EnumType.STONE){
-                world.setBlockState(pos, BlockRegister.silverFishStone.getDefaultState());
-            }
-        }
+//        for (int i = 0; i < random.nextInt(40); i++) {
+//            BlockPos pos = WorldGenHelper.randomXZPos(chunkX, chunkZ, random.nextInt(64) + 1, random);
+//            IBlockState blockState = WorldHelper.getBlockState(world, pos);
+//            if (blockState.getBlock() == Blocks.STONE && blockState.getValue(BlockStone.VARIANT) == BlockStone.EnumType.STONE){
+//                world.setBlockState(pos, BlockRegister.silverFishStone.getDefaultState());
+//            }
+//        }
     }
 
 }
