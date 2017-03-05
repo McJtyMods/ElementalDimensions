32027b93d93da249cf6242f8d8c1162e1ab7f975
McJty <mcjty1@gmail.com>
2017-03-05 20:07:21 +0100

    0.1.0

--- src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
+++ src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
@@ -44,7 +44,7 @@ public class ElementalDimensions {
 
     public static final String MODID = "elementaldimensions";
     public static final String MODNAME = "Elemental Dimensions";
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "0.1.0";
 
     public static final String MIN_ELECCORE = "1.6.358";
     public static final String MIN_FORGE10_VER = "12.18.1.2082";
