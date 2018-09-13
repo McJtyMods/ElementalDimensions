a2d37471be018535db832c43042a641cb2b274ac
McJty <mcjty1@gmail.com>
2018-09-13 05:39:53 +0200

    0.3.1

--- src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
+++ src/main/java/bitmovers/elementaldimensions/ElementalDimensions.java
@@ -45,7 +45,7 @@ public class ElementalDimensions implements ModBase {
 
     public static final String MODID = "elementaldimensions";
     public static final String MODNAME = "Elemental Dimensions";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
     public static final String MIN_MCJTYLIB_VER = "3.0.0";
 
     public static final String MIN_ELECCORE = "1.7.409";
